af154a16351b90ed90159446622d4458f6e5bff7
Zakaria Kebairia <4.kebairia@gmail.com>
2021-10-30 06:37:26 +0100

    adding intro code

--- intro/intro.py
+++ intro/intro.py
@@ -3,14 +3,13 @@ from manim import *
 
 
 class Intro(Scene):
-
     def construct(self):
-        circle = Circle(radius=2).set_color(WHITE)
-        pi = MathTex(r'\pi', font_size=140).scale(2)
-        sub_text = Text("3Point14", font_size=90)
-
-        # Scenes
-        self.play(Create(circle), run_time=1)
+        c = Circle(color=WHITE)
+        pi = MathTex('\pi', font_size=140)
+        sub_text = Text("3Point14", font_size=35).shift(DOWN * 1.3)
+        self.play(Create(c), run_time=2)
+        self.wait(1.3)
+        self.play(DrawBorderThenFill(pi), run_time=1.5)
         self.wait()
-        self.play(DrawBorderThenFill(pi))
+        self.play(Write(sub_text))
         self.wait(2)
