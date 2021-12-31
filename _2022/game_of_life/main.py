0a5b90af13a7f2fe7fcb109b7eadbf07345ee0ce
Zakaria Kebairia <4.kebairia@gmail.com>
2021-12-31 10:02:28 +0100

    initial code

---
+++ _2022/game_of_life/main.py
@@ -0,0 +1,5 @@
+from manim import *
+# - Any live cell with fewer than two live neighbours dies, as if by needs caused by underpopulation.
+# - Any live cell with more than three live neighbours dies, as if by overcrowding.
+# - Any live cell with two or three live neighbours lives, unchanged, to the next generation.
+# - Any dead cell with exactly three live neighbours becomes a live cell.
