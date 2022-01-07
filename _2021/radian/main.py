3d7af19f0ac31880def04adf7c899cd484fb91e2
Zakaria Kebairia <4.kebairia@gmail.com>
2022-01-07 21:59:33 +0100

    First version of radian Scene

--- _2021/radian/main.py
+++ _2021/radian/main.py
@@ -2,17 +2,28 @@ from manim import *
 import math
 
 class Radian(Scene):
-    _fg = "#1d2021"
+    _fg = "#171717"
+    _red = "#cc1d24"
     _radius = 2
     _dot_radius = 0.065
     _width = 2
     def construct(self):
         self.camera.background_color = "#e1e1e1"
-        c = Circle(radius=2, stroke_color=BLACK)
-        center = Dot(color=RED).move_to(c.get_center())
-        line = Line(start=c.get_center(), end=c.get_right(), stroke_color=RED)
-        self.add(c, center, line)
-        self.play(Rotate(line, -PI/2, about_point=c.get_right()))
-        arc = Arc(radius=2, angle=1, stroke_color=RED)
-        self.play(Swap(line, arc))
+        def draw_arc(length):
+            arc = Arc(radius=self._radius, angle=length, stroke_color=self._red)
+            self.play(Write(arc))
+            return arc
+
+        circle = Circle(radius=2, stroke_color=BLACK)
+        center = Dot(color=RED).move_to(circle.get_center())
+        diameter = Line(start=circle.get_center(), end=circle.get_right(), stroke_color=RED)
+        self.add(circle, center, diameter)
+
+        tangent_line = TangentLine(circle, alpha=0, length=diameter.get_length(), color=RED).shift(UP*1)
+        self.play(Rotate(diameter, -PI/2, about_point=circle.get_right()))
+        self.remove(diameter)
+        self.add(tangent_line)
+
+        arc = Arc(radius=self._radius, angle=1, stroke_color=self._red)
+        self.play(Transform(tangent_line, arc), run_time=0.5)
         self.wait()
