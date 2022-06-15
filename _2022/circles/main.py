52859c61b190ae10307d287199206c86ca88c1f2
Zakaria Kebairia <4.kebairia@gmail.com>
2022-06-15 13:53:17 +0100

    updating circles projects

--- _2022/circles/main.py
+++ _2022/circles/main.py
@@ -1,10 +1,14 @@
 from manim import *
 import math
-
-config.background_color = "#455D3E"
+# config.frame_height = 16
+# config.frame_width = 9
+# config.pixel_height = 720
+# config.pixel_width = 1366
+config.background_color = "#045b7d"
 class Circles(Scene):
     n = 250
     _dot_radius = 0.015
+    _circle_radius = 3
     _BLUE = "#045b7d"
     _GREEN = "#455D3E"
     _YELLOW = "#fdaa26"
@@ -13,7 +17,7 @@ class Circles(Scene):
     _BLACK = "#3a3a3a"
     def construct(self):
         def draw_shape(k, n, radius):
-            c = Circle(radius=radius, stroke_color=self._WHITE)
+            c = Circle(radius=radius, stroke_color=self._WHITE).to_edge(RIGHT)
             pos = [i*(360/n)*DEGREES for i in range(n)]
             lines = VGroup(*[Line(c.point_at_angle(i), c.point_at_angle(k*i%n), stroke_color=self._WHITE, stroke_width=1) 
                 for i in pos])
@@ -21,15 +25,18 @@ class Circles(Scene):
             return shape
 
         shapes = VGroup()
-        for i in range(2, 251):
-            shape = draw_shape(i, 250, 2.5)
-            # self.play(Create(shape[0]))
-            # self.wait()
-            # self.play(Create(shape[1], lag_ratio=0.2), run_time=4)
-            # self.wait()
+        for i in range(2, 102):
+            shape = draw_shape(i, 250, self._circle_radius)
             shapes.add(shape)
-            # self.play(FadeOut(shape))
-        # self.play(Create(shapes[0]), run_time=0.25)
+
+        bg_shapes = VGroup(*[shape.copy().set_style(stroke_opacity=0.2) for shape in shapes])
+        bg_shapes.arrange_in_grid(
+                cols=10, rows=10, buff=1).scale_to_fit_width(
+                        config.frame_width - (config.frame_width/2)).scale_to_fit_width(config.frame_height - 4)
+        self.add(bg_shapes.to_edge(LEFT))
         for i in range(1, len(shapes)):
-            self.play(Uncreate(shapes[i-1]), Create(shapes[i]), run_time=0.2)
+            self.play(Uncreate(shapes[i-1]), Create(shapes[i]), 
+                    bg_shapes[i-1].animate.set_style(stroke_opacity=0.2),
+                    bg_shapes[i].animate.set_style(stroke_opacity=1),
+                    run_time=0.2)
         self.wait(2)
